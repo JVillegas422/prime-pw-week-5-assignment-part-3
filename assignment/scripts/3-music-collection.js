0a939046165ee93c833c311fbda9274e181224bb
Juan Villegas <jvillegas422@gmail.com>
2022-09-17 23:52:16 -0500

    Created new album function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,15 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+// Created function to add new album
+// to empty array.
+function addToCollection( title, artist, yearPublished ) {
+    let album = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished
+    }
+    collection.push(album);
+    return album;
+}
